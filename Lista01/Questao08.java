5b5158c6ef4076be66c1f794370e249a48db1bea
ovitorj <augustoovitor@gmail.com>
2018-09-05 16:25:59 -0300

    Add files via upload

--- Lista01/Questao08.java
+++ Lista01/Questao08.java
@@ -2,13 +2,14 @@ import java.util.*;
 
 public class Questao08{
 
-   /*
+  /*
       Método na classe Pilha para limpar a pilha
-      
+         
       public void limpa(){
-      topo = null;
-      qtde = 0;
-      }
+         while(topo != null){
+            topo = topo.prox;
+            qtde--;
+          }
    */
 
    public static void main (String [] args){
@@ -22,5 +23,6 @@ public class Questao08{
       
       p.limpa();
       System.out.println(p.peek());
+      System.out.println(p.quantidade());
    }
 }
