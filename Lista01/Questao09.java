d3a8e7a9679a439bf675e51c8397dc468872a9f6
ovitorj <augustoovitor@gmail.com>
2018-09-05 16:40:34 -0300

    Add files via upload

--- Lista01/Questao09.java
+++ Lista01/Questao09.java
@@ -1,25 +1,39 @@
 import java.util.*;
 
 public class Questao09{
-
+/* 
+ * Procedimento que apaga os elementos da fila f
+ * 
    public static void questao09(CFila f){
       int qdte= f.quantidade();
-      
       for(int i=0; i<qdte; i++){
          f.desenfileira();
       }
       
    }//endQuestao09
+   */
+   
+   /* Procedimento que apaga os elementos da fila contido na classe CFila
+    * 
+    * 
+    public void limpa(){
+      while(frente != tras){
+         frente = frente.prox;
+         qtde--;
+      }
+      */
    
    public static void main (String [] args){
+      
       CFila f = new CFila();
       int a=0;
+      
       //for para preencher a fila com 10 elementos
       for(int i=0; i<10; i++){
          f.enfileira(++a);
       }
          
-      questao09(f);
+      f.limpa();
       
       System.out.println(f.peek());
    }
