0aea5c9b3963744cce026b0f194f13953c11ac05
ovitorj <augustoovitor@gmail.com>
2018-09-14 10:05:13 -0300

    Add files via upload

---
+++ TrabAEDII/Questao28.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Questao28{
+   
+   public static void furaFila(CFila f, Object item){
+      //Definicao das variaveis
+      int qtde = f.quantidade();
+      CFila faux = new CFila();
+     
+      //for para preencher uma fila auxiliar com os valores de f
+      for(int i=0; i<qtde; i++){
+         faux.enfileira(f.desenfileira());
+      }
+      
+      //enfileirar o item recebido
+      f.enfileira(item);
+      
+      //for para voltar com os valores para f
+      for(int i=0; i<qtde; i++){
+         f.enfileira(faux.desenfileira());
+      }
+      
+   }
+   public static void preenche(CFila f){
+      int a=0;
+      for(int i=0; i<10; i++){
+         f.enfileira(++a);
+      }
+   }
+   
+   public static void main(String [] args){
+      
+      CFila f = new CFila();
+      preenche(f);
+      furaFila(f,999);
+   }
+}
