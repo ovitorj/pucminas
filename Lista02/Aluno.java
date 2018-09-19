11d653ff064794f2f1a500fd3e0b4c97eee1416c
ovitorj <augustoovitor@gmail.com>
2018-09-19 17:53:12 -0300

    Add files via upload

---
+++ Lista02/Aluno.java
@@ -0,0 +1,11 @@
+public class Aluno{
+
+   public String nome;
+   public int matricula;
+   
+   public Aluno(String valorNome, int valorMatricula){
+      nome = valorNome;
+      matricula = valorMatricula;   
+   }
+
+}
